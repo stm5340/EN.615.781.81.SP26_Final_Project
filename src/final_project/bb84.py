36c175d747589c97b0d27ad80b7660c04ef8e979
Shawn McLain <shawnmclain1024@gmail.com>
2026-04-03 23:52:40 -0400

    Adding main() function to verify behavior

--- src/final_project/bb84.py
+++ src/final_project/bb84.py
@@ -5,6 +5,7 @@ import numpy as np
 from numpy.typing import NDArray
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator, AerJob
+from itertools import compress
 
 SEED = None  # Overwrite with fixed value for reproducible results
 rng = np.random.default_rng(SEED)
@@ -12,41 +13,18 @@ rng = np.random.default_rng(SEED)
 BASIS_CHOICES = ["Z", "X"]
 BIT_CHOICES = [0, 1]
 
-@dataclass
-class SimulationInputs:
-    """
-    Data class containing all chosen inputs by Alice and Bob for each message in the simulation
-    """
-
-    input_bits: NDArray[np.int64]
-    alice_bases: NDArray[np.str_]
-    bob_bases: NDArray[np.str_]
-    circuits: NDArray[np.object_]
-
-    def discard_data(self):
-        """
-        Remove data where Alice and Bob don't use the same basis
-        """
-        raw = deepcopy(self)  # Provide copy of unaltered data if desired
-        self.input_bits = self.input_bits[self.alice_bases == self.bob_bases]
-        self.alice_bases = self.alice_bases[self.alice_bases == self.bob_bases]
-        self.bob_bases = self.bob_bases[self.alice_bases == self.bob_bases]
-        self.circuits = self.circuits[self.alice_bases == self.bob_bases]
-        return self, raw
-
-
 class BB84(QuantumCircuit):
     """
     Class that implements a BB84 circuit with no eavesdropping using the qiskit library's QuantumCircuit class
     """
 
-    input_bit: int
-    alice_basis: str
-    bob_basis: str
+    input_bit: int = None
+    alice_basis: str = None
+    bob_basis: str = None
 
     def __init__(self, input_bit: int, alice_basis: str, bob_basis: str):
 
-        self = super().__init__(1, 1, name="BB84")
+        super().__init__(1, 1, name="BB84")
 
         if input_bit != 0 and input_bit != 1:
             raise ValueError("Input bit must take the classical state of '0' or '1'")
@@ -67,8 +45,35 @@ class BB84(QuantumCircuit):
         else:
             raise ValueError("Bob must choose an input basis of 'Z' or 'X'")
 
+        self.input_bit = input_bit
+        self.alice_basis = alice_basis
+        self.bob_basis = bob_basis
+
         self.measure(0, 0)  # Measure the first register (zeroth index)
 
+
+@dataclass
+class SimulationInputs:
+    """
+    Data class containing all chosen inputs by Alice and Bob for each message in the simulation
+    """
+
+    input_bits: NDArray[np.int64]
+    alice_bases: NDArray[np.str_]
+    bob_bases: NDArray[np.str_]
+    circuits: List[BB84]
+
+    def discard_data(self) -> Tuple[object, object]:
+        """
+        Remove data where Alice and Bob don't use the same basis
+        """
+        raw = deepcopy(self)  # Provide copy of unaltered data if desired
+        mask = self.alice_bases == self.bob_bases
+        self.input_bits = self.input_bits[mask]
+        self.alice_bases = self.alice_bases[mask]
+        self.bob_bases = self.bob_bases[mask]
+        self.circuits = list(compress(self.circuits, mask))
+        return self, raw
 
 def create_simulation_space(n_messages: int) -> SimulationInputs:
     """
@@ -83,13 +88,10 @@ def create_simulation_space(n_messages: int) -> SimulationInputs:
         input_bits,
         alice_bases,
         bob_bases,
-        np.array(
-            [
-                BB84(input_bits[ii], alice_bases[ii], bob_bases[ii])
-                for ii in range(n_messages)
-            ],
-            dtype=np.object_
-        ),
+        [
+            BB84(input_bits[ii], alice_bases[ii], bob_bases[ii])
+            for ii in range(n_messages)
+        ],
     )
 
     return sim_inputs
@@ -100,7 +102,7 @@ def simulate_communication(sim_inputs: SimulationInputs) -> NDArray[np.int64]:
     Given a set of simulation inputs, simulate communication between Alice and Bob, and record the bit Bob measure's
     """
     backend = AerSimulator()
-    circuits: List[BB84] = sim_inputs.circuits.tolist()
+    circuits: List[BB84] = sim_inputs.circuits
     num_messages = len(circuits)
     results: AerJob = backend.run(transpile(circuits, backend), shots=1, memory=True).result()
     ouput_bits = []
@@ -121,14 +123,16 @@ def bit_error_rate(input_bits: NDArray[np.int64], output_bits: NDArray[np.int64]
     return np.sum(input_bits.flatten() != output_bits.flatten()) / np.size(input_bits)
 
 
-if __name__ == '__main__':
-
+def main() -> None:
     sim_inputs = create_simulation_space(n_messages=128)
     output_bits = simulate_communication(sim_inputs=sim_inputs)
     output_bits = output_bits[sim_inputs.alice_bases == sim_inputs.bob_bases]
     sim_inputs.discard_data()  # Remove inputs where Alice and Bob don't have the same basis
     qber = bit_error_rate(input_bits=sim_inputs.input_bits, output_bits=output_bits)
+    print(f"Computed quantum bit error rate in simulation: {qber}")
 
 
 
+if __name__ == '__main__':
 
+    main()
